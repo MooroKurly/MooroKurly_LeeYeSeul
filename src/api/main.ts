8ecdb6b4f2a42c4b5c07cba0d2f7a4f447c92b60
seu11ee <yslee2504@naver.com>
2021-06-20 04:45:00 +0900

    add banners api

--- src/api/main.ts
+++ src/api/main.ts
@@ -3,13 +3,14 @@ import { ISale, ISaleOutputDTO } from "../interface/ISale"
 const router = express.Router();
 
 import Sale from "../model/Sale";
-// import "../models/Category"
+import SpecialSale from "../model/SpecialSale";
+import Banner from "../model/Banner";
 
 router.get(
-    "/sales/:saleIndex",
+    "/sales",
     async(req: Request, res: Response) => {
         try{
-            var sales = await Sale.find().where('saleIndex').equals(Number(req.params.saleIndex)).populate("product");
+            var sales = await Sale.find().where('saleIndex').equals(Number(req.query.id)).populate("product");
             var salesOutput: ISaleOutputDTO[]=[];
             console.log(sales);
             if (!sales){
@@ -31,5 +32,41 @@ router.get(
         }
     }
 )
+
+router.get(
+    "/sales/specialPrice",
+    async(req: Request, res: Response) => {
+        try{
+            var specialSales = await SpecialSale.find().populate("product");
+            console.log(specialSales);
+            if (!specialSales){
+                return res.status(204).send();
+            }
+            
+            res.json({msg:"특가 정보 조회 성공",specialPrices:specialSales});
+        } catch (error){
+            console.error(error.message);
+            res.status(500).send({message:"Server Error"});
+        }
+    }
+)
+
+router.get(
+    "/banners",
+    async(req: Request, res: Response) => {
+        try{
+            //find()하면 배열로 리턴
+            const banners = await Banner.find();
+            if (!banners){
+                return res.status(204).send();
+            }
+            res.json({msg:"배너 조회 성공",banners:banners});
+        } catch (error){
+            console.error(error.message);
+            res.status(500).send("Server Error");
+        }
+    }
+)
+
 
 module.exports = router;
